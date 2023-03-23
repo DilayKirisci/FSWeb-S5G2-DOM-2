8508b0d551765136589d41c09ad734c4b04d0a42
Dilay Kirisci <isik.kirisci@gmail.com>
2023-03-23 12:49:29 +0300

    "final version"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,48 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
 // document.querySelector("h1").addEventListener("click",function(e){
 // alert("Bana tıkladın!")
 // });
 
-
 // Kodlar buraya gelecek!
+
+const links = document.querySelectorAll("a");
+function linkAnimation(e) {
+	e.preventDefault();
+	e.target.style.backgroundColor = "pink";
+}
+links.forEach((link) => {
+	link.addEventListener("mouseover", linkAnimation);
+});
+
+const images = document.querySelectorAll("img");
+function changeSize(e) {
+	e.preventDefault();
+	e.target.style.width = "400px";
+	e.target.style.height = "400px";
+}
+images.forEach((image) => {
+	image.addEventListener("mouseover", changeSize);
+});
+
+const welcome = document.querySelector(".intro h2");
+welcome.addEventListener("select", changeSize);
+
+window.addEventListener("dblclick");
+
+document.addEventListener("dblclick", function () {
+	const elem = document.documentElement;
+	const requestFullScreen = elem.requestFullscreen;
+
+	if (requestFullScreen) {
+		requestFullScreen.call(elem);
+	}
+});
+
+const btns = document.querySelectorAll(".btn");
+btns.forEach((btn) =>
+	btn.addEventListener("click", () => {
+		btn.textContent = "Simdi Al!";
+	})
+);
